31496359e354490942221c23aa4b4df26ad9c91a
Ken Thooptong <ken.thooptong@gmail.com>
2019-03-19 22:30:37 +0000

    create validation, ask user for a store name

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -18,9 +18,9 @@ class Employee < ActiveRecord::Base
 end
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 30)
-@store1.employees.create(first_name: "Jack", last_name: "Watson", hourly_rate: 30)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 50)
+@store1.employees.create(first_name: "Jack", last_name: "Watson", hourly_rate: 70)
 
-@store2.employees.create(first_name: "Jane", last_name: "Donelly", hourly_rate: 10)
-@store2.employees.create(first_name: "Jim", last_name: "Huber", hourly_rate: 40)
-@store2.employees.create(first_name: "Rose", last_name: "Potter", hourly_rate: 40)
+@store2.employees.create(first_name: "Jane", last_name: "Donelly", hourly_rate: 80)
+@store2.employees.create(first_name: "Jim", last_name: "Huber", hourly_rate: 80)
+@store2.employees.create(first_name: "Rose", last_name: "Potter", hourly_rate: 90)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,20 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+    validates :first_name,  presence: true
+    validates :last_name,   presence: true
+    validates :hourly_rate, inclusion: 40..200
+    validates :store_id,    presence: true
+end
+
+class Store < ActiveRecord::Base
+    validates :name, length: { minimum: 3 }
+    validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+end
+
+##Create a store based on user's answer
+puts "Enter a store name"
+@user_answer = gets.chomp
+@user_store = Store.new(name: @user_answer)
+@user_store.save
